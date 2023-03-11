f01c4bf4b562bbac21e3188e4267fe0b61daf233
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2023-03-11 09:41:00 -0300

    Agregando WebDriverManager32

--- src/test/java/runner/runner.java
+++ src/test/java/runner/runner.java
@@ -1,19 +1,13 @@
 package runner;
 
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import pages.BasePage;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = "steps", monochrome = true, tags = "@Test")
 
 public class runner {
-    // @AfterClass
-    // public static void cleanDriver(){
-    // BasePage.closeBrowser();
-    // }
 
 }
